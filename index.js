53188198ea4c7ff0b2812ec19e285840565ca293
dotfold <jamesmcnamee@gmail.com>
2016-08-12 18:12:16 +1000

    Initialize Kibana Plugin (generator-kibana-plugin v1.0.1)

---
+++ index.js
@@ -0,0 +1,30 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      app: {
+        title: 'Reactor',
+        description: 'lets just use react',
+        main: 'plugins/reactor/app'
+      },
+      hacks: [
+        'plugins/reactor/hack'
+      ]
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+
+  });
+};
